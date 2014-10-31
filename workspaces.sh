59531b3e8716a14c8bfbb6f09928b58c061b22f5
molly <ecg@unseen.is>
2014-10-31 01:55:13 +0000

    small updates

--- workspaces.sh
+++ workspaces.sh
@@ -4,17 +4,17 @@
 for i in {2..11}
 do
 SPACE=$(bspc control --get-status | cut -d ":" -f $i)
-	if [[ "$SPACE" == *u* ]]
+	if [[ "$SPACE" == *u* ]] #urgent desktop color
 	then
-		echo -ne "\${color4}"
-	elif [[ "$SPACE" == *O* ]]
+		echo -ne "\${color4}" 
+	elif [[ "$SPACE" == *O* ]] #focused desktop color
 	then
-		echo -ne "\${color1}"
+		echo -ne "\${color1}" #unfocused, non-free desktop color
 	elif [[ "$SPACE" == *o* ]]
 	then
 		echo -ne "\${color3}"
 	else
-		echo -ne "\${color2}"
+		echo -ne "\${color2}" #free desktop color
 	fi
-echo -ne "${SPACE:1} "
+echo -ne "${SPACE:1} " #name of desktop as set in bspwmrc
 done
